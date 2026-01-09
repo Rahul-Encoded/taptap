94136cdbd5b8002099418f506cd51ad563193728
Rahul-Encoded <rahulm03@outlook.com>
2026-01-09 23:56:33 +0530

    Created FileName component so that user can set Doc name

---
+++ app/EditorComponents/NavbarComponents/FileName.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useAtom } from "jotai";
+import { fileNameAtom } from "./utils/atoms/atoms";
+
+export default function FileName(){
+    const [fileName, setFileName] = useAtom(fileNameAtom);
+    
+
+    return(
+        
+        <textarea rows={1} value={fileName} onChange={(e) => setFileName(e.target.value)} className="w-auto bg-transparent border-2 border-transparent hover:border-purple-800 focus:border-purple-800 focus:outline-none resize-none rounded-md transition-colors px-2 py-1 text-2xl font-semibold"/>
+        
+    )
+}
