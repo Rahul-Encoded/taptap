b45885f1247f84db28f0efa0c68b053eda8a9563
Rahul-Encoded <rahulm03@outlook.com>
2026-01-09 23:55:43 +0530

    Added Navbar and TipTap Components in the main app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Tiptap from "./EditorComponents/tiptap";
+import NavBar from "./EditorComponents/NavbarComponents/Navbar";
 
 export default function Home() {
   return (
+    <>
+    <NavBar />
     <Tiptap />
+    </>
   );
 }
