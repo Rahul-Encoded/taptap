113694f05ae8a40f94f5c865fbd875b335879756
Rahul-Encoded <rahulm03@outlook.com>
2026-01-10 23:14:10 +0530

    added necessary css

--- app/EditorComponents/NavbarComponents/StatusDropdownrow.tsx
+++ app/EditorComponents/NavbarComponents/StatusDropdownrow.tsx
@@ -1,10 +1,10 @@
 export default function StatusDropdownrow({text, icon, description, onClick}: {text: string, icon: React.ReactNode, description: string, onClick: () => void}){
     return(
-        <div className="flex gap-2 items-center cursor-pointer" onClick={onClick}>
+        <div className="flex gap-2 items-center cursor-pointer rounded-lg p-2 w-full" onClick={onClick}>
             {icon}
             <div className="flex flex-col gap-1">
                 <span>{text}</span>
-                <span>{description}</span>
+                <span className="text-xs text-foreground">{description}</span>
             </div>
         </div>
     )
