609b76de40152ab614adbcf1ea5d6320492bc086
Rahul-Encoded <rahulm03@outlook.com>
2026-01-10 23:14:28 +0530

    necessary css based on selected atom

--- app/EditorComponents/NavbarComponents/StatusSelector.tsx
+++ app/EditorComponents/NavbarComponents/StatusSelector.tsx
@@ -8,11 +8,12 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { SquarePen } from "lucide-react"
-import { statusBadge } from "./utils/constants/statusBadge"
+import { statusBadges } from "./utils/constants/statusBadge"
 import StatusDropdownrow from "./StatusDropdownrow"
 import { StatusBadge as StatusBadgeInterface } from "./utils/interfaces/statusBadge"
 import { useAtom } from "jotai"
 import { statusAtom } from "./utils/atoms/atoms"
+import { cn } from "@/lib/utils"
 
 export default function StatusSelector(){
     const [status, setStatus] = useAtom(statusAtom)
@@ -20,12 +21,12 @@ export default function StatusSelector(){
     return(
         <DropdownMenu>
             <DropdownMenuTrigger className="border-2 border-transparent hover:border-border rounded-full p-1 cursor-pointer "><SquarePen className="w-4 h-4"></SquarePen></DropdownMenuTrigger>
-            <DropdownMenuContent className="w-40">
+            <DropdownMenuContent className="w-60">
             <DropdownMenuLabel>Change Status</DropdownMenuLabel>
             <DropdownMenuSeparator></DropdownMenuSeparator>
-            {statusBadge.map((status: StatusBadgeInterface) => (
-                <DropdownMenuItem key={status.text}>
-                    <StatusDropdownrow text={status.text} icon={status.icon} description={status.description} onClick={() => setStatus(status)} />
+            {statusBadges.map((statusBadge: StatusBadgeInterface) => (
+                <DropdownMenuItem key={statusBadge.text} className={cn(statusBadge.text === status.text ? "bg-purple-400/50 text-purple-500" : "")}>
+                    <StatusDropdownrow text={statusBadge.text} icon={statusBadge.icon} description={statusBadge.description} onClick={() => setStatus(statusBadge)} />
                 </DropdownMenuItem>
             ))}
             </DropdownMenuContent>
