d97351be11273182d73eaf9f453968c00fddde72
Rahul-Encoded <rahulm03@outlook.com>
2026-01-12 16:57:29 +0530

    Created ModeToggle Button

---
+++ app/EditorComponents/NavbarComponents/ModeToggle.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  const handleModeToggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
+  return (
+    <Button variant="outline" onClick={handleModeToggle}>
+      <Sun className="h-2 w-2 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-2 w-2 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
