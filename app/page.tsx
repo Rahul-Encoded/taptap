80e4707e374933b6855ee8924359f31f5349a9be
Rahul-Encoded <rahulm03@outlook.com>
2026-01-14 05:30:54 +0530

    updated component usage to be consistent with the component name

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Tiptap from "./EditorComponents/tiptap";
 import NavBar from "./EditorComponents/NavbarComponents/Navbar";
+import TipTapEditor from "./EditorComponents/Tiptap";
 
 export default function Home() {
   return (
     <>
     <NavBar />
     <div className="m-20 border-none p-4">
-    <Tiptap />
+    <TipTapEditor />
     </div>
     </>
   );
