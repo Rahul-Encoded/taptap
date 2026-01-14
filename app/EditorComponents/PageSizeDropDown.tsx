6620f9369005c879b9ee5c3e47d764ae4bf59b46
Rahul-Encoded <rahulm03@outlook.com>
2026-01-14 13:44:43 +0530

    PageSizeDropDown component

---
+++ app/EditorComponents/PageSizeDropDown.tsx
@@ -0,0 +1,23 @@
+import { DropdownMenu, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useAtom } from "jotai";
+import { pageSizeAtom } from "./NavbarComponents/utils/atoms/atoms";
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+
+import { ChevronDownIcon } from "lucide-react";
+
+export default function PageSizeDropDown() {
+    const [pageSize, setPageSize] = useAtom(pageSizeAtom)
+    const sizes = ["A4", "A3", "A5", "Legal", "Letter", "Tabloid"]
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center gap-2 border border-border rounded-md p-2 text-sm">{pageSize}<ChevronDownIcon size={12}/></DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-white">
+                <DropdownMenuGroup>
+                {sizes.map((size) => (
+                    <DropdownMenuItem key={size} onSelect={() => {setPageSize(size)}}>{size}</DropdownMenuItem>
+                ))}
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
