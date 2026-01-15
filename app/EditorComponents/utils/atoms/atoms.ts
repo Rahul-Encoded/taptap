589970f3dba062f39c8c531940ca3b01bcd30503
Rahul-Encoded <rahulm03@outlook.com>
2026-01-15 14:38:16 +0530

    Removed Header and footer atoms

--- app/EditorComponents/utils/atoms/atoms.ts
+++ app/EditorComponents/utils/atoms/atoms.ts
@@ -7,12 +7,6 @@ export const fileNameAtom = atom("Untitled");
 // Status atom
 export const statusAtom = atom(statusBadges[0]);
 
-//Header atom
-export const headerAtom = atom("");
-
-//Footer atom
-export const footerAtom = atom("");
-
 // Page Size types and atom
 export type PageSizeKey = "A4" | "A3" | "A5" | "LETTER" | "LEGAL" | "TABLOID";
 export const pageSizeAtom = atom<PageSizeKey>("A4");
